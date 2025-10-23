bd944efa3783dce6fb80958456805c91824df74b
Rubia <rubia.alice.m.s@gmail.com>
2025-10-22 23:06:21 -0300

    feat: Adiciona WordInfo como data ao TrieNode

--- src/TestCompressedTrie.py
+++ src/TestCompressedTrie.py
@@ -2,17 +2,12 @@ from CompressedTrie import CompressedTrie
 
 if __name__ == "__main__":
     trie = CompressedTrie()
-    trie.insert("computer")
-    trie.insert("compute")
-    trie.insert("computing")
-    trie.print_trie()
-    print(trie.search("computer"))  # True
-    print(trie.search("compute"))   # True
-    print(trie.search("computing")) # True
-    print(trie.search("commute"))   # False
-    print(trie.search("comp"))      # False
-    trie.insert("commute")
-    trie.print_trie()
-    print(trie.search("commute"))   # True
+    with open('noticias/business/001.txt', 'r', encoding='utf-8') as file:
+        for line in file:
+            cleaned_line: str = line.strip()
+            words: list[str] = cleaned_line.split()
+            for word in words:
+                word = word.lower().strip()
+                trie.insert(word, 'noticias/business/001.txt')
 
-    
+    print(trie.jsonafy())

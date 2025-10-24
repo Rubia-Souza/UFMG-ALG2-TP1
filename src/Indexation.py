9a1ca156b31b2feef447928f20e92012598d4fe2
Rubia <rubia.alice.m.s@gmail.com>
2025-10-23 21:17:21 -0300

    !refactor: Altera indexação para desconsiderar caracteres especiais

--- src/Indexation.py
+++ src/Indexation.py
@@ -1,7 +1,7 @@
-import re
 import os
 from src.CompressedTrie import CompressedTrie
 from src.StringUtils import remove_punctuation_from_beginning_and_end
+from src.Cleaner import clean_word
 
 def create_compressed_trie_from_file(file_path: str) -> CompressedTrie | None:
     trie: CompressedTrie = CompressedTrie()
@@ -11,39 +11,12 @@ def create_compressed_trie_from_file(file_path: str) -> CompressedTrie | None:
                 cleaned_line: str = line.strip()
                 words: list[str] = cleaned_line.split()
                 for word in words:
-                    word = word.lower().strip()
-                    word = remove_punctuation_from_beginning_and_end(word)
+                    word = clean_word(word)
                     trie.insert(word, file_path)
         return trie
     except Exception as e:
         print(f"[ERROR]: Could not create trie from file {file_path}. Exception: {e}")
         return None
-
-def save_trie_in_file(trie: CompressedTrie, file_path: str) -> None:
-    try:
-        with open(file_path, 'w', encoding='utf-8') as file:
-            trie_data: str = trie.serialize()
-            file.write(trie_data)
-    except Exception as e:
-        print(f"[ERROR]: Could not save trie to file {file_path}. Exception: {e}")
-
-def create_compressed_tries_to_folder(folder_path: str, output_folder_path: str) -> None:
-    if not os.path.exists(output_folder_path):
-        os.makedirs(output_folder_path)
-    
-    for filename in os.listdir(folder_path):
-        output_file_name: str = f"{os.path.splitext(filename)[0]}_trie.txt"
-        output_file_path: str = os.path.join(output_folder_path, output_file_name)
-        if os.path.isfile(output_file_path):
-            continue
-
-        if filename.endswith('.txt'):
-            file_path: str = os.path.join(folder_path, filename)
-            trie: CompressedTrie | None = create_compressed_trie_from_file(file_path)
-            if trie is not None:
-                save_trie_in_file(trie, output_file_path)
-            else:
-                print(f"[ERROR]: Trie creation failed for file {file_path}.")
 
 def create_compressed_trie_from_reversed_index() -> CompressedTrie:
     data: list[dict] = []
@@ -83,8 +56,7 @@ def create_compressed_trie_from_raw_files() -> None:
                             cleaned_line: str = line.strip()
                             words: list[str] = cleaned_line.split()
                             for word in words:
-                                word = word.lower().strip()
-                                word = remove_punctuation_from_beginning_and_end(word)
+                                word = clean_word(word)
                                 trie.insert(word, file_path)
                 except Exception as e:
                     print(f"[ERROR]: Could not read file {file_path} to insert words into trie: {e}")
